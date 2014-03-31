8a01cef694701deee5b7115c45d29507f3d26632
pennatil <pennatil@student.ethz.ch>
2014-03-31 23:05:56 +0200

    Continued chapter 3, other
    - Continued work on chapter 3
    - Couple of fixes in the building scripts
    - Added \listoftodos to main.tex
    - Updated all chapters, as single pdf and an entire one
    - Added AutomatedRun.sh in order to build everything in one shot

---
+++ MAINs/AutomatedRun.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#Automated run
+
+cd "/Users/Lucas/Documents/ETH/2nd Semester/Analysis II/Notes/MAINs/"
+
+./BuildAll.sh
+
+./BuildChapterByChapter.sh
--- MAINs/BuildChapterByChapter.sh
+++ MAINs/BuildChapterByChapter.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # My first Script
 
 # echo "Hello World!"
